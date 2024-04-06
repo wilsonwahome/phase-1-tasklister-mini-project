58f2ac9c851a9a2684da1370535219f80c203278
wahome <wahomewgitonga@gmail.com>
2024-04-06 20:08:16 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_task.value);
+    form.reset()
+  })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  let editBtn = document.createElement('button');
+
+  btn.addEventListener('click', removeItem)
+  btn.textContent = 'x'
+  editBtn.addEventListener('click', editItem);
+  editBtn.textContent = 'Edit';
+
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  p.appendChild(editBtn);
+  
+  document.querySelector('#list').appendChild(p)
+}
+
+function removeItem (e){
+  e.target.parentNode.remove()
+}
+
+function editItem(e) {
+  let newText = prompt('Enter new task description:');
+  if (newText !== null) {
+    e.target.parentNode.textContent = newText;
+  }
+}
